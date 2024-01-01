a14836ebf8be91734e7bf022b833cbe832730c54
Jefry Lagrange <jefry.reyes@gmail.com>
2024-01-01 20:03:31 +0000

    Create snippet file and fill it with necessary information.

--- eje-tests.el
+++ eje-tests.el
@@ -1,4 +1,5 @@
-
+;; Example exanded env
+;; ((yas-indent-line 'fixed) (yas-java-test-methods '("void testa()" "void testb()" "void testc()")))
 
 ;; Only works in java-ts-mode
 
@@ -6,13 +7,26 @@
 ;; use yas-expand-snippet
 
 (defun eje-create-test-for-class ()
+  (interactive)
   (let* ((class-name (eje--get-class-name))
-         (test-path (eje--find-test-path class-name (eje--get-package))))
+         (package (eje--get-package))
+         ;;(test-path (eje--find-test-path class-name (eje--get-package)))
+         (test-path "/home/jlagrang/pruba.java")
+         (snippet (yas-lookup-snippet "unittest" 'java-ts-mode))
+         (public-methods (eje--get-public-methods))
+         )
     ;; create file - or just switch to buffer without saving
     ;; switch to buffer
     (if (eje--test-class? class-name)
         (error "Attempting to create test for a test")
-      (progn (switch-to-buffer (find-file-noselect test-path))))
+      (progn (switch-to-buffer (find-file-noselect test-path))
+             (yas-expand-snippet snippet
+                                 (point-min)
+                                 (point-max)
+                                 '( (yas-indent-line 'fixed)
+                                    (yas-java-test-methods public-methods)
+                                    (yas-java-package package)
+                                    (yas-java-class class-name)))))
     )
   )
 
@@ -20,6 +34,23 @@
 (defun eje--find-test-path (class-name package)
   "Given a string representing a java class-name package, return the path for testing that class."
   )
+
+(defun eje--get-public-methods  ()
+  (let* ((root-node (treesit-buffer-root-node))
+         (query '((program (class_declaration  (class_body (method_declaration (modifiers "public") @modifier_node (identifier) @method_name) )))))
+         (query-result (treesit-query-capture root-node query)))
+    (cl-remove-duplicates
+      (cl-map 'list
+              'treesit-node-text
+              (eje--get-nodes 'method_name query-result))
+              :test 'string=)))
+
+
+(defun eje--get-nodes (node-type query-result)
+  "Returns a list of nodes given a node-type and a query-result"
+  (cl-map 'list (lambda (x) (cdr x))
+                (cl-remove-if-not (lambda (x) (eq (car x) node-type))
+                                  query-result)))
 
 (defun eje--get-package ()
   "Get the package name from a Java class in the current buffer."
@@ -31,8 +62,8 @@
 
 (defun eje--get-node-text (query)
   (let* ((root-node (treesit-buffer-root-node))
-         (query-value (treesit-query-capture root-node query)))
-    (treesit-node-text (cdr (car query-value)))))
+         (query-result (treesit-query-capture root-node query)))
+    (treesit-node-text (cdr (car query-result)))))
 
 (defun eje--test-class? (class-name)
   "Return t if current buffer is a test class"
