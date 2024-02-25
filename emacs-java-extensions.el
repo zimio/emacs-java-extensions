b73c7a8e790afc95007249dfbf19faaa31d572d1
Jefry Lagrange <jefry.reyes@gmail.com>
2024-02-25 10:24:50 +0000

    Add ability to run test on a file file and a single method.

--- emacs-java-extensions.el
+++ emacs-java-extensions.el
@@ -20,6 +20,7 @@
                                    (yas-java-test-methods public-methods)
                                    (yas-java-package package)
                                    (yas-java-class class-name)))))))
+
 (defun eje-go-to-test-class ()
   (interactive)
   (let* ((class-name (eje--get-class-name))
@@ -27,14 +28,41 @@
          (test-path (eje--find-test-path class-name (eje--get-package))))
     (find-file (eje--add-wildcard-path test-path) t)))
 
+(defun eje-run-test-file ()
+  (interactive)
+  (eje--projectile-test-project (eje--get-class-name)))
+
+(defun eje-run-test-method ()
+  (interactive)
+  (let ((method (eje--get-current-method)))
+    (when method
+      (eje--projectile-test-project (concat (eje--get-class-name)
+                                            "#"
+                                            method)))))
+
+(defun eje--get-current-method ()
+  (eje--get-node-text '((method_declaration (identifier) @x))
+                      (treesit-defun-at-point)))
+
+(defun eje--projectile-test-project (test-arg)
+  (let ((command (projectile-test-command (projectile-compilation-dir)))
+        (command-map (if (projectile--cache-project-commands-p) projectile-test-cmd-map)))
+    (when (cl-search "mvn" command)
+      (projectile--run-project-cmd (concat command " -Dtest=" test-arg)
+                                   command-map
+                                   :show-prompt nil
+                                   :prompt-prefix "Test command: "
+                                   :save-buffers t
+                                   :use-comint-mode projectile-test-use-comint-mode))))
+
 (defun eje--add-wildcard-path (test-path)
   (let ((split-path (split-string test-path "/")))
     (concat
-        (string-join (butlast split-path) "/")
-        "/*"
-        (car (split-string (car (last split-path)) "\\."))
-        "*"
-        ".java")))
+     (string-join (butlast split-path) "/")
+     "/*"
+     (car (split-string (car (last split-path)) "\\."))
+     "*"
+     ".java")))
 
 (defun eje--find-test-path (class-name package)
   "Given a string representing a java class-name package, return the path for testing that class."
@@ -66,15 +94,16 @@
 
 (defun eje--get-package ()
   "Get the package name from a Java class in the current buffer."
-  (eje--get-node-text '((program (package_declaration (scoped_identifier) @x)))))
+  (eje--get-node-text '((program (package_declaration (scoped_identifier) @x)))
+                      (treesit-buffer-root-node)))
 
 (defun eje--get-class-name ()
   "Get the class name from a Java class declaration in the current buffer using Tree-sitter."
-  (eje--get-node-text '((program (class_declaration (identifier) @x)))))
+  (eje--get-node-text '((program (class_declaration (identifier) @x)))
+                      (treesit-buffer-root-node)))
 
-(defun eje--get-node-text (query)
-  (let* ((root-node (treesit-buffer-root-node))
-         (query-result (treesit-query-capture root-node query)))
+(defun eje--get-node-text (query node)
+  (let ((query-result (treesit-query-capture node query)))
     (treesit-node-text (cdr (car query-result)))))
 
 (defun eje--test-class? (class-name)
